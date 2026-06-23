42eb96745ac4350c892025b88c4b47e2d37e95d7
Anton Makarevich <anton.makarevich@gmail.com>
2026-06-23 22:51:31 +0200

    feat: add placeholder images

--- src/MakaMek.MapEditor/Models/ToolItem.cs
+++ src/MakaMek.MapEditor/Models/ToolItem.cs
@@ -2,9 +2,11 @@ using Sanet.MakaMek.Map.Models.Terrains;
 
 namespace Sanet.MakaMek.MapEditor.Models;
 
-public class ToolItem(string displayName, ToolType type, Terrain? terrain = null)
+public class ToolItem(string displayName, ToolType type, Terrain? terrain = null, string? imagePath = null)
 {
     public string DisplayName { get; } = displayName;
     public ToolType Type { get; } = type;
     public Terrain? Terrain { get; } = terrain;
+    public string? ImagePath { get; } = imagePath;
+    public bool HasImagePath => ImagePath is not null;
 }
